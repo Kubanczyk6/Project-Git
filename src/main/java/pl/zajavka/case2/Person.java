225e49fb0caac1017dcee6ec945ab2857062e802
Jakub Grądzki <jakub.gradzki96@gmail.com>
2023-03-28 20:34:09 +0200

    Person class modification 1

--- src/main/java/pl/zajavka/case2/Person.java
+++ src/main/java/pl/zajavka/case2/Person.java
@@ -2,8 +2,19 @@ package pl.zajavka.case2;
 
 import java.math.BigDecimal;
 
-record Person(String name, BigDecimal salary) {
+record Person(String name, String surname, BigDecimal salary) {
 
+    public String whatIsYourJob() {
+        return "Software developer!";
+    }
+
+    public String whatDoYouLike() {
+        return "Strawberries";
+    }
+
+    public Boolean doYouHaveABike() {
+        return true;
+    }
     public BigDecimal howMuchDoYouEarn() {
         return salary;
     }
